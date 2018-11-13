d272341073a43ee5db7ee69d2dec04a457d5fc68
hejack0207 <hejack0207@sina.com>
2018-11-13 11:48:34 +0800

    fix

--- schema.mk
+++ schema.mk
@@ -7,11 +7,10 @@ all: $(SQLFILES) $(OUTPUTSQL)
 init:
 	jo -p local="$$(jo -p url="gjzspt_demo2/Oe123qwe###@10.0.52.1:1521/orcl")" dev="$$(jo -p url="gjzspt/12345678@192.168.21.249:1521/gjzs")" test="$$(jo url="gjzspt_demo2/Oe123qwe###@10.0.52.8:1521/orcl")" >db.json
 
-asms.sql:
-	svn cat $(SVN_ROOT)/sofn-server/sofn-asms-service/src/main/resources/sql/asms-upgrade-2018-11-12.sql >$@
+asms-upgrade.sql ales-upgrade.sql ads-upgrade.sql tts-upgrade.sql dgap-upgrade.sql:
 
-dgap.sql:
-	svn cat $(SVN_ROOT)/sofn-server/sofn-dgap-service/src/main/resources/sql/dgap-upgrade.sql >$@
+%-upgrade.sql:
+	svn cat $(SVN_ROOT)/sofn-server/sofn-%*-service/src/main/resources/sql/schema-upgrade.sql >$@
 
 schema-update.sql: bjdbclean/bj7/tts.sql bjdbclean/bj7/ads-upgrade-2018-11-12.sql bjdbclean/bj7/ales.sql bjdbclean/bj7/asms-upgrade-2018-11-12.sql
 	#cat $(SQLFILES) >$@
