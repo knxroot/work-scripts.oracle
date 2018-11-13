c7f649e8a3bc62e33b34e9ea3c34a509000bc0e3
hejack0207 <hejack0207@sina.com>
2018-11-13 09:59:51 +0800

    fix

--- schema.mk
+++ schema.mk
@@ -1,9 +1,12 @@
 SHELL:=/usr/bin/zsh
+SQLFILES:=asms.sql
 
-all:
+all: $(SQLFILES)
 
-sqlfiles:
-	svn export https://192.168.21.251/svn/CodeRepository/GuoJiaZhuiSuPingTai/BusinessSystem/sofn-server/sofn-asms-service/src/main/resources/sql/asms-upgrade-2018-11-12.sql
+asms.sql:
+	svn cat https://192.168.21.251/svn/CodeRepository/GuoJiaZhuiSuPingTai/BusinessSystem/sofn-server/sofn-asms-service/src/main/resources/sql/asms-upgrade-2018-11-12.sql >$@
 
+clean:
+	rm $(SQLFILES)
 
-.PHONY: all
+.PHONY: all clean
