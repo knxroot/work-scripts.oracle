83b6867c606eeac98a0c1b1473d8e6ebedbef73e
hejack0207 <hejack0207@sina.com>
2018-11-20 18:09:10 +0800

    fix

--- schema.mk
+++ schema.mk
@@ -7,7 +7,7 @@ all: $(SQLFILES) $(OUTPUTSQL)
 
 init: 
 	#jo -p local="$$(jo -p url="gjzspt_demo2/Oe123qwe###@10.0.52.1:1521/orcl")" dev="$$(jo -p url="gjzspt/12345678@192.168.21.249:1521/gjzs")" test="$$(jo url="gjzspt_demo2/Oe123qwe###@10.0.52.8:1521/orcl")" >db.json
-	jj -p -i schema.json -o schema.json -v "2018-11-11" sync-date
+	jj -p -i schema.json -o schema.json -v "2018-11-19" sync-date
 
 db.json: rdp/ds-all.json
 	cp $< $@
@@ -16,17 +16,14 @@ db.json: rdp/ds-all.json
 %-upgrade.sql:
 	typeset -A prjs=(ads sofn-ads-service asms sofn-asms-service ales sofn-ales-service tts sofn-tts-service-branch)
 	prj=$${prjs[$*]}
-	#print -l $$prj
 	files=($$(comm -23 =(svn ls $(SVN_ROOT)/sofn-server/$$prj/src/main/resources/sql | sort) =(svn ls $(SVN_ROOT)/sofn-server/$$prj/src/main/resources/sql@{$$(command jq -r '.sqls.$*."last-fetch-date"' schema.json)} | sort)))
 	rm -rf schema-updates/$*
 	svn co --depth empty $(SVN_ROOT)/sofn-server/$$prj/src/main/resources/sql schema-updates/$*
-	#eval svn update schema-updates/$*/{$${(j:,:)files}}
-	svn update schema-updates/$*/$$files
-	eval cat schema-updates/$*/*(.on) >$@
+	[[ -n $$files ]] && svn update schema-updates/$*/$$files
+	[[ -n $$files ]] && eval cat schema-updates/$*/*(.on) >$@
 	print -n '\n' >>$@
 	jj -p -i schema.json -o schema.json -v $(shell date +%F) sqls.$*.last-fetch-date
 
-#schema-update.sql: bjdbclean/bj7/tts.sql bjdbclean/bj7/ads-upgrade-2018-11-12.sql bjdbclean/bj7/ales.sql bjdbclean/bj7/asms-upgrade-2018-11-12.sql
 schema-update.sql: $(SQLFILES)
 	cat $^ >$@
 	dos2unix $@
@@ -48,8 +45,8 @@ checkdb-%: db.json
 
 clean:
 	rm $(SQLFILES)
+	rm schema-update.sql
 	rm -rf schema-updates
-	rm db.json
 
 #$(call checkdb,profile)
 define checkdb
